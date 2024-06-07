07dc113a435bd6a94940cc0e7455e84467d37987
adonistuarez <adonistuarezv@gmail.com>
2024-06-07 01:12:43 -0500

    Delete src directory

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.recuperacion {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.recuperacion to javafx.fxml;
-    exports com.example.recuperacion;
-}
